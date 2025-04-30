805b8c022a2c0014d1640888677a77cf805dffe0
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-01 01:11:28 +0600

    feat: design new level and added cherries

--- scense/main_character.gd
+++ scense/main_character.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 400.0
+const JUMP_VELOCITY = -900.0
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
+	# character movements 
+	if(velocity.x > 1  || velocity.x > -1): 
+		sprite_2d.animation = "running"
+	else: 
+		sprite_2d.animation = "default"
+		
+		
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		sprite_2d.animation= "jumping"
+		
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 50)
+
+	# Flip sprite based on direction
+	sprite_2d.flip_h = velocity.x < 0
 
 	move_and_slide()
