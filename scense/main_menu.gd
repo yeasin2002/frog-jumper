f0c4d236e923976d7e62e4a8092a3311dada524c
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-01 15:01:54 +0600

    feat: added main menu and level choise

---
+++ scense/main_menu.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+func _on_lvl_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scense/level-1.tscn")
+
+
+
+
+func _on_lvl_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scense/level-2.tscn")
