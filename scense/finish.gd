1e82c9c152e5db5e71b8b858af69101c2966de61
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-01 15:17:48 +0600

    feat: created both level sync, and level navigarion while finished

---
+++ scense/finish.gd
@@ -0,0 +1,6 @@
+extends Area2D
+@export var targetLevel  = PackedScene
+
+func _on_body_entered(body: Node2D) -> void:
+	if(body.name == "CharacterBody2D"):
+		get_tree().change_scene_to_packed(targetLevel)
