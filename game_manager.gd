8185bb81fe32ff80ab4ebe845f3bf8fe4c5ca794
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-01 11:38:23 +0600

    feat: updated Game UI, added point view and showing collected point in UI

--- game_manager.gd
+++ game_manager.gd
@@ -1,8 +1,11 @@
 extends Node
+@onready var show_point: Label = %showPoint
+
 
 var point  = 0 
 
 func incrementPoint():
 	point+=1
-	print(point) 
+	print(point)
+	show_point.text = str(point)
 		
