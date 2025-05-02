5fc843839bacf78c4f35bd4129e1cbcffe8c9faf
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-02 12:35:00 +0600

    fix: level import and scenes change warning

--- scense/finish.gd
+++ scense/finish.gd
@@ -1,6 +1,10 @@
 extends Area2D
-@export var targetLevel  = PackedScene
+@export var targetLevel: PackedScene
 
 func _on_body_entered(body: Node2D) -> void:
-	if(body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_packed(targetLevel)
+	if body.name == "CharacterBody2D":
+		# Use call_deferred to change scene after physics processing is complete
+		call_deferred("_change_scene")
+
+func _change_scene() -> void:
+	get_tree().change_scene_to_packed(targetLevel)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,10 +2,10 @@ extends Node
 
 
 func _on_lvl_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://scense/level-1.tscn")
+	get_tree().change_scene_to_file("res://scense/levels/level-1.tscn")
 
 
 
 
 func _on_lvl_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scense/level-2.tscn")
+	get_tree().change_scene_to_file("res://scense/levels/level-2.tscn")
